3c7a16a67d3f53d5d04ab980243c02c3b0e652d1
Diego Uribe <dfuribez@unal.eu.co>
2019-12-14 11:39:27 -0500

    Lee y grafica los datos de ambos acelerómetros. La trama ahora termina con un final de línea.

--- telemetria/main.py
+++ telemetria/main.py
@@ -25,10 +25,13 @@ class Gui(QMainWindow, gui_class):
 
         self.n = 0
 
-        self.acel_x_all = []
-        self.acel_y_all = []
-        self.acel_z_all = []
-
+        self.acel_x1_all = []
+        self.acel_y1_all = []
+        self.acel_z1_all = []
+        
+        self.acel_x2_all = []
+        self.acel_y2_all = []
+        self.acel_z2_all = []
 
         self.pg_window_acel = pg.GraphicsWindow()
         self.pg_window_vel = pg.GraphicsWindow()
@@ -53,68 +56,120 @@ class Gui(QMainWindow, gui_class):
 
         l_buffer = len(buffer.split("*"))
 
-        if not l_buffer >= 3:
+        if not l_buffer >= 6:
             return
         try:
-            x = float(buffer.split("*")[1])
-            y = float(buffer.split("*")[2])
+            splitted = buffer.split("*")
+            x1 = float(splitted[0])
+            y1 = float(splitted[1])
+            z1 = float(splitted[2])
+            
+            x2 = float(splitted[3])
+            y2 = float(splitted[4])
+            z2 = float(splitted[5])
         except:
             print("Error convirtiendo a numero")
             return
         x = self.n
         self.n += 1
 
-        print("ploting", x, y, d)
+        print("ploting", x1, y1, d)
 
-        self.acel_x_all.append(x)
-        self.acel_z_all.append(y+ 300)
-        self.acel_y_all.append(y)
+
+        self.acel_x1_all.append(x1)
+        self.acel_z1_all.append(z1)
+        self.acel_y1_all.append(y1)
+        
+        self.acel_x2_all.append(x2)
+        self.acel_y2_all.append(y2)
+        self.acel_z2_all.append(z2)
+        
+        
 
         self.pg_plot_acel_x.clear()
         self.pg_plot_acel_y.clear()
         self.pg_plot_vel.clear()
 
-        a = self.pg_plot_acel_x.plot(title="Acelerometro 1")
         self.pg_plot_acel_x.addLegend()
         self.pg_plot_acel_x.plot(
-            self.acel_x_all[d:],
-            self.acel_y_all[d:],
+            range(self.n),
+            self.acel_x1_all[d:],
             #symbol="o",
             style=QtCore.Qt.DotLine,
             # pen=pg.mkPen(color=(255, 100, 100)),
-            pen=(1, 2),
+            pen=(1, 3),
             antialias = True,
-            name="adfg"
+            name="eje x"
         )
-
+        
+        
         self.pg_plot_acel_x.plot(
-            self.acel_x_all[d:],
-            self.acel_z_all[d:],
+            range(self.n),
+            self.acel_y1_all[d:],
             #symbol="o",
             style=QtCore.Qt.DotLine,
-            # pen=pg.mkPen(color=(255, 255, 255)),
-            pen=(2,2),
+            # pen=pg.mkPen(color=(255, 100, 100)),
+            pen=(2, 3),
             antialias = True,
-            name="f"
+            name="eje y"
+        )
+        
+        self.pg_plot_acel_x.plot(
+            range(self.n),
+            self.acel_z1_all[d:],
+            #symbol="o",
+            style=QtCore.Qt.DotLine,
+            # pen=pg.mkPen(color=(255, 100, 100)),
+            pen=(3, 3),
+            antialias = True,
+            name="eje z"
         )
 
+
+
+        self.pg_plot_acel_y.addLegend()
         self.pg_plot_acel_y.plot(
-            self.acel_x_all[d:],
-            self.acel_y_all[d:],
+            range(self.n),
+            self.acel_x2_all[d:],
             #symbol="o",
             style=QtCore.Qt.DotLine,
-            pen=pg.mkPen(color=(255, 100, 100)),
-            antialias = True
+            # pen=pg.mkPen(color=(255, 100, 100)),
+            pen=(1, 3),
+            antialias = True,
+            name="eje x"
         )
-
-        self.pg_plot_vel.plot(
-            self.acel_x_all[d:],
-            self.acel_y_all[d:],
+        
+        
+        self.pg_plot_acel_y.plot(
+            range(self.n),
+            self.acel_y2_all[d:],
             #symbol="o",
             style=QtCore.Qt.DotLine,
-            pen=pg.mkPen(color=(255, 100, 100)),
-            antialias = True
+            # pen=pg.mkPen(color=(255, 100, 100)),
+            pen=(2, 3),
+            antialias = True,
+            name="eje y"
         )
+        
+        self.pg_plot_acel_y.plot(
+            range(self.n),
+            self.acel_z2_all[d:],
+            #symbol="o",
+            style=QtCore.Qt.DotLine,
+            # pen=pg.mkPen(color=(255, 100, 100)),
+            pen=(3, 3),
+            antialias = True,
+            name="eje z"
+        )
+#
+#        self.pg_plot_vel.plot(
+#            self.acel_x_all[d:],
+#            self.acel_y_all[d:],
+#            #symbol="o",
+#            style=QtCore.Qt.DotLine,
+#            pen=pg.mkPen(color=(255, 100, 100)),
+#            antialias = True
+#        )
     
     def conectar(self):
         puerto = str(self.qt_puerto_lineedit.text())
@@ -160,10 +215,11 @@ class SerialThread(QThread):
         while True:
             try:
                 byte = self.ser.read(1).decode("utf8")
-                if byte == ";":
+                if byte == "\n":
                     self.signal.emit(buffer)
                     self.guardar(buffer)
                     time.sleep(0.1)
+                    print(buffer)
                     buffer = ""
                 else:
                     buffer += byte
